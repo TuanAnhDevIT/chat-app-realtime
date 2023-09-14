239a3bf4090092b3a4e3c82c70717665a4456782
DangTranTuanAnh <dangtrantuananh.2002@gmail.com>
2023-09-14 14:01:21 +0700

    first commit

---
+++ index.js
@@ -0,0 +1,27 @@
+const express = require('express')
+const app = express()
+
+const http = require('http')
+const server = http.createServer(app)
+const { Server } = require('socket.io')
+
+const io = new Server(server)
+
+app.get('/', (req, res) => {
+    // res.send('hello world vjp')
+    res.sendFile(__dirname + '/index.html')
+})
+
+io.on('connection', (socket) => {
+    console.log('user connected')
+    socket.on('on-chat', data => {
+        console.log({
+            data
+        })
+        io.emit('user-chat', data)
+    })
+})
+
+server.listen(3000, () => {
+    console.log('listen on port 3000')
+})
